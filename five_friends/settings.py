f75b5053f0ac1d62f6dc8544e46e32895249cb58
fabritsius <antonfabritsius@gmail.com>
2019-07-02 22:05:32 +0300

    feat: Use HTTPS requests only

--- five_friends/settings.py
+++ five_friends/settings.py
@@ -22,6 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SERVER_SECRET_KEY')
 
+# redirect all non-HTTPS requests to HTTPS 
+SECURE_SSL_REDIRECT = True
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
